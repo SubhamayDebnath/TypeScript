cdf59e8fd8a72549a76fe1168aacab7637171d8b
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-06-14 20:24:29 +0530

    added

--- variables.js
+++ variables.js
@@ -1,2 +1,8 @@
 var username = 'Hello';
-console.log(username);
+// console.log(username);
+username.toLowerCase();
+// console.log(username.toUpperCase());
+var userId = 12344.5;
+console.log(userId + 0.5);
+var isLoggedIn = false;
+console.log(isLoggedIn);
--- variables.ts
+++ variables.ts
@@ -1,2 +1,10 @@
-let username:string = 'Hello';
-console.log(username);
+let username: string = 'Hello';
+// console.log(username);
+username.toLowerCase();
+// console.log(username.toUpperCase());
+
+let userId: number = 12344.5;
+// console.log(userId+0.5);
+
+const isLoggedIn: boolean = false;
+// console.log(isLoggedIn);
